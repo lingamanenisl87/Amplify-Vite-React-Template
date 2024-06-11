a81e939dbaab83494d979d240bb64091f25bbdf3
Dileepk999 <k4873343@gmail.com>
2024-06-11 12:17:19 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,9 @@ import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 import { Authenticator } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
-
-const client = generateClient<Schema>();
-
 import { Authenticator } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
+const client = generateClient<Schema>();
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
